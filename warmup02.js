103c91aa6813ad0867cf479a97e62014a989777c
mestiri77 <faroukmestiri77@gamil.com>
2024-04-29 11:19:49 +0100

    warmup2 correction

--- warmup02.js
+++ warmup02.js
@@ -1,42 +1,36 @@
 /**
  * 1-write a function that returns the product of any number by 3.
  */
-function productOf3(n){
-  
-
-}
+let productOf3 = n => n * 3;
 
 /**
  * 2-complete the following function multiply to return the result of multiplying x and y
  */
-function multiply(x,y){
-  
-
-}
-
+const multiply = (x, y) => x * y;
 
 /**
  * 3-complete the following function that Convert Fahrenheit to Celsius if the module for converting it
  */
-function fahrenheitToCelsius(fahrenheit){
-}
-
+const fahrenheitToCelsius = fahrenheit => `${(fahrenheit - 32) * 5 / 9} °C`;
 
 /**
  * 4-Define a function named "sqArea", and make it display square area if you knew the length of its side.
  */
-function sqArea(length) {
-
-}
- /* 5 - Write a function called scoreToGrade that accepts a number as a parameter and returns a string representing a letter grade corresponding to that score.
+const sqArea = length => length ** 2; // ** is exponant
+/* 5 - Write a function called scoreToGrade that accepts a number as a parameter and returns a string representing a letter grade corresponding to that score.
  */
-function scoreToGrade(score){
-}
+const scoreToGrade = score => {
+  if (score === 20) return "A+";
+  else if (score >= 18) return "A-";
+  else if (score >= 15) return "B+";
+  else if (score >= 12) return "C+";
+  else if (score >= 10) return "C";
+  else return "F";
+};
 /**
  * 6 - Write a function sameLength that accepts two strings as arguments, and returns true if those strings have the same length, and false otherwise.
  */
-function sameLength(string1, string2) {      
-}
+const sameLength=(string1, string2)=> string1.length === string2.length
 /**
  *! 1 Write a function named helloWorld that:
  *! takes 1 argument, a language code (e.g. "fr", "es", "en")
@@ -50,5 +44,10 @@ function sameLength(string1, string2) {
  *! @return {string}
  */
 
-function helloWorld(){
+const helloWorld=(str)=> {
+    if(str === "fr") return "Bonjour tout le monde"
+    else if (str === "es") return "Hola, mundo como estas"
+    else if (str === "tn") return "Aslema nes kol"
+    else if ( str === "") return "Hello world"
+    else if ( str === "en") return "Hello world"
 }
